e3c2320f17198e1217556369e605af4a4ad8d928
lhywell <lihuiyin128@163.com>
2017-12-25 15:33:48 +0800

    no message

--- node/example/test.js
+++ node/example/test.js
@@ -7,6 +7,7 @@ console.log(process.env.npm_package_config_port)
 console.log(process.env.npm_package_name)
 console.log(process.env.npm_package_version)
 console.log(process.env.npm_package_scripts_start)
-
+console.log(process.env.npm_lifecycle_event)
+console.log(process.env.npm_config_access)
 
 index.printMsg()
